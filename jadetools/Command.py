e894977835dd0619354f0dc7dc181cb1a659d180
James Bellinger <jbellinger@mail.icecube.wisc.edu>
2019-10-28 13:34:40 -0500

    Add tool to control off/on for system

--- jadetools/Command.py
+++ jadetools/Command.py
@@ -1,3 +1,15 @@
+# Command.py (.base)
+import sys
+# IMPORT_utils.py
+# Assumes "import sys"
+import site
+import json
+import urllib.parse
+import subprocess
+import copy
+import os
+import uuid
+
 # IMPORT CODE_utils.py
 #####
 # Define some constants
@@ -223,3 +235,48 @@ def flagBundleStatus(key, newstatus):
               + 'for key ' + str(key) + ' : ' + str(outp))
     return outp, erro, code
 #
+
+DEBUGIT = False
+
+
+########################################################
+# Define Phases for Main.  In this case, all I care about
+# is control of the NERSC and Local control systems.  It
+# turns out NERSC has some issues with keeping the power
+# on (thank you, PG&E), and it is handy to be able to
+# turn everything off or on again.
+
+def Phase1(pcarg):
+    #
+    lcarg = pcarg
+    geturl = copy.deepcopy(basicposturl)
+    geturl.append(targetsetdumpstatus + mangle(lcarg))
+    answer = massage(getoutputsimplecommandtimeout(geturl, 1))
+    if 'OK' not in answer:
+        print('Phase 1 failure with', geturl)
+        return
+    #
+    if lcarg == 'Drain':
+        lcarg = 'DrainNERSC'
+    geturl = copy.deepcopy(basicposturl)
+    geturl.append(targetsetnerscstatus + mangle(lcarg))
+    answer = massage(getoutputsimplecommandtimeout(geturl, 1))
+    if 'OK' not in answer:
+        print('Phase 1 failure with', geturl)
+    return
+
+###############
+# Main
+
+if len(sys.argv) == 1:
+    print('No command given.  Run, Halt, Drain are possible')
+    sys.exit(0)
+
+carg = str(sys.argv[1])
+
+if carg not in ['Run', 'Halt', 'Drain']:
+    print('Command ' + carg + ' is not in Run, Halt, Drain; bailing')
+    sys.exit(1)
+
+Phase1(carg)
+sys.exit(0)
