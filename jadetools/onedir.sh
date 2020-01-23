2bba3c2e4d1d13f8dd265007d91681e3b547c539
James Bellinger <jbellinger@mail.icecube.wisc.edu>
2020-01-23 11:36:47 -0600

    Add REST server interface, test

--- jadetools/onedir.sh
+++ jadetools/onedir.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 # arguments:
-# $1 => directory
-# $2 => condor job #
+# $1 => real directory
+# $2 => ideal directory
+#IGNORE FOR NOW # $3 => condor job #
 
 # 0'th step:  sanity!
-if [[ "$2" == "" ]]; then echo "$0 needs directory and condor_job_id"; exit 1; fi
+#IGNORE FOR NOW if [[ "$2" == "" ]]; then echo "$0 needs directory and condor_job_id"; exit 1; fi
+if [[ "$1" == "" ]]; then echo "$0 needs directory"; exit 1; fi
 directory=$1
+idealDirectory=$2
 if [[ ! -d ${directory} ]]; then echo "$1 is not a visible directory"; exit 1; fi
 
-# First curl to let the REST server know what we're doing with this directory
+# First curl to let the REST server know what we're doing with this directory. 
+# The table in question is WorkingTable
 # New status = Preparing
+manglefile=$( echo "${idealDirectory}" | sed 's@/@+++@g' )
+manglecom=$( echo "${manglefile}@Preparing" )
+/usr/bin/curl -sS -X POST -H Content-Type:application/x-www-form-urlencoded http://archivecontrol.wipac.wisc.edu:80/glue/workupdate/${manglecom}
+if [[ $? != 0 ]]
+  then
+    echo "FAILURE w/ set Preparing"
+    exit 1
+  fi
 
 # Second setup
 cd ~jade/lta
 source env/bin/activate
 
 # Third do the load
-./ltacmd catalog load --path ${directory}
+echo ./ltacmd catalog load --path ${directory}
 if [[ $? != 0 ]]
   then
      # try again
-     ./ltacmd catalog load --path ${directory}
+echo     ./ltacmd catalog load --path ${directory}
      if [[ $? != 0 ]]
        then
           echo "FAILURE with catalog load of ${directory}"
@@ -30,9 +42,21 @@ if [[ $? != 0 ]]
 
 # Fourth setup the request
 
-./ltacmd request new --source WIPAC --dest NERSC --path ${directory}
+echo ./ltacmd request new --source WIPAC --dest NERSC --path ${directory}
+if [[ $? != 0 ]]
+  then
+    echo "FAILURE w/ request"
+    exit 1
+  fi
 
 # Fifth curl to let the REST server know we are done with this directory
-# New status = Loaded
+# New status = Picked
+manglecom=$( echo "${manglefile}@Picked" )
+/usr/bin/curl -sS -X POST -H Content-Type:application/x-www-form-urlencoded http://archivecontrol.wipac.wisc.edu:80/glue/workupdate/${manglecom}
+if [[ $? != 0 ]]
+  then
+    echo "FAILURE w/ set Picked"
+    exit 1
+  fi
 
 exit 0
