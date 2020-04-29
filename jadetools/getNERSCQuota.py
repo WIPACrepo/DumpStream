c04f540d8a958a1cff44624bfb969526fe0536ab
James Bellinger <jbellinger@icecube.wisc.edu>
2020-04-29 10:24:59 -0500

    Consolidate code; complete but not entirely tested

--- jadetools/getNERSCQuota.py
+++ jadetools/getNERSCQuota.py
@@ -4,16 +4,31 @@ import json
 import requests
 import Utils as U
 
+#####
+#
 class BearerAuth(requests.auth.AuthBase):
     ''' Translate the LTA REST server token into something useful.
         This relies on the "requests" package
         This initialzes with a string token, and on call returns
         a Bearer token for the requests call to use '''
-    def __init__(self, token):
-        self.token = token
+    def __init__(self, stoken):
+        self.token = stoken
     def __call__(self, r):
         r.headers["authorization"] = "Bearer " + self.token
         return r
+
+#####
+#
+def readConfig(fileName='Dump.json'):
+    '''  Read the configuration -- which filesystems and what limits
+        from the specified file '''
+    try:
+        with open(fileName) as f:
+            data = json.load(f)
+        return data
+    except:
+        print('readConfig failed to read', fileName)
+        return None
 
 #####
 #
@@ -38,19 +53,22 @@ def normalizeAnswer(quotaString):
 
 #####
 #
-class checkNERSC():
+class checkExternals():
     ''' Encapsulate the NERSC quota check code '''
     def __init__(self, config, name='service-token'):
         self.getLTAToken(name)
         self.config = config
+        if os.path.isfile('/bin/du'):
+            self.execdu = '/bin/du'
+        else:
+            self.execdu = '/usr/bin/du'
+        if os.path.isfile('/bin/df'):
+            self.execdf = '/bin/df'
+        else:
+            self.execdf = '/usr/bin/df'
     #
     def __call__(self, filesystem):
-        _, used, avail = self.checkQuotaNERSC()
-        if used < 0 or avail < 0:
-            return False
-        if used > self.config['FRACTION_NERSC_QUOTA'] * avail:
-            return False
-        return True
+        return self.go_nogo()
     #
     def getLTAToken(self, name):
         ''' Read the LTA REST server token from file "name"; return the same '''
@@ -63,10 +81,19 @@ class checkNERSC():
             sys.exit(1)
     #
     def checkQuotaNERSC(self):
+        ''' get the cscratch1 use from NERSC--are we too full? '''
+        _, used, avail = self.checkQuotaNERSC()
+        if used < 0 or avail < 0:
+            return False
+        if used > self.config['FRACTION_NERSC_QUOTA'] * avail:
+            return False
+        return True
+    #
+    def getQuotaNERSC(self):
         ''' Retrieve the NERSC cscratch1 space usage and quota and most recent time '''
         r3 = requests.get('https://lta.icecube.aq/status/site_move_verifier', auth=BearerAuth(self.token))
         if r3.status_code != 200:
-            print('checkQuotaNERSC: site_move_verifier check', r3.status_code)
+            print('getQuotaNERSC: site_move_verifier check', r3.status_code)
             return '', -1, -1
         details = r3.json()
         latestTime = ''
@@ -85,58 +112,78 @@ class checkNERSC():
         return latestTime, normalizeAnswer(latestUsed), normalizeAnswer(latestQuota)
 
 
-class checkDu():
-    ''' Encapsulate the du checks '''
-    def __init__(self, config):
-        if os.path.isfile('/bin/du'):
-            self.exec = '/bin/du'
-        else:
-            self.exec = '/usr/bin/du'
-        self.confg = config
-    #
-    def FetchDuPercent(self, target):
-        ''' Execute a "du" and parse the output for the % used
-            Warning.  du is not in the same location on different OS
+    def FetchDfPercent(self, target):
+        ''' Execute a "df" and parse the output for the % used
+            Warning.  df is not in the same location on different OS
             versions.  For example, we have /bin/du on jade03, and
             /usr/bin/du on the cluster nodes.
             The arguments are the target directory/filesystem, and
             an optional location other than /bin/du if you want to run
             on something other than jade03  '''
-        cmd = [self.exec, target]
+        cmd = [self.execdf, target]
         answer, error, code = U.getoutputerrorsimplecommand(cmd, 1)
         if code != 0 or len(error) > 2:
-            print('FetchDuPercent failed', error, answer, code)
-            return 200
+            print('FetchDfPercent failed', error, answer, code)
+            return False
         www = answer.split('%')
         if len(www) != 3:
-            print('FetchDuPercent got more than one answer', answer)
-            return 200
+            print('FetchDfPercent got more than one answer', answer)
+            return False
         www = answer.split()        # Ignore lines; assumes first answer is correct
         for wword in www:
             if '%' in wword and 'Use' not in wword:
                 wx = wword.split('%')
                 try:
-                    return float(wx[0])
+                    flans = float(wx[0])
+                    break
                 except:
-                    print('FetchDuPercent could not make a number out of', wx[0])
-                    return 200
-        return 200
-    
-#####
-#
-def readConfig(fileName='Dump.json'):
-    '''  Read the configuration -- which filesystems and what limits
-        from the specified file '''
-    try:
-        with open(fileName) as f:
-            data = json.load(f)
-        return data
-    except:
-        print('readConfig failed to read', fileName)
-        return None
+                    print('FetchDfPercent could not make a number out of', wx[0])
+                    return False
+        if flans > self.config['FRACTION_FS']:
+            return False
+        return True
+
+    def checkLocalSpace(self):
+        ''' Execute a du on the stage and out areas; should not exceed limit '''
+        cmd = [self.execdu, '-h', self.config['STAGE_ROOT'] + '/bundler_out']
+        answer, error, code = U.getoutputerrorsimplecommand(cmd, 1)
+        if code != 0 or len(error) > 2:
+            print('checkLocalSpace failed', error, answer, code, 'bundler_out')
+            return False
+        try:
+            outsize = int(answer.split()[0])
+        except:
+            print('checkLocalSpace failed to get out size', answer)
+            return False
+        if outsize/1024 > self.config['SIZE_BUFFER_GB']:
+            return False
+        cmd = [self.execdu, '-h', self.config['STAGE_ROOT'] + '/bundler_stage']
+        answer, error, code = U.getoutputerrorsimplecommand(cmd, 1)
+        if code != 0 or len(error) > 2:
+            print('checkLocalSpace failed', error, answer, code, 'bundler_stage')
+            return False
+        try:
+            stagesize = int(answer.split()[0])
+        except:
+            print('checkLocalSpace failed to get stage size', answer)
+            return False
+        if (stagesize + outsize)/1024 > self.config['SIZE_ALL_LOCAL_BUNDLE_GB']:
+            return False
+        return True
+
+    def go_nogo(self):
+        ''' Put all the calls in a single place.  Returns False if we are blocked '''
+        if not self.FetchDfPercent(self.config['ROOT']):
+            return False
+        if not self.FetchDfPercent(self.config['STAGE_ROOT']):
+            return False
+        if not self.checkLocalSpace():
+            return False
+        if not self.checkQuotaNERSC():
+            return False
+        return True
+
+
+x = readConfig()
+print(x['STAGE_ROOT'])
  
-token = getLTAToken('service-token')
-    
-time, size, maximum = checkQuotaNERSC(token)
-    
-percentlfs7 = FetchDuPercent('/mnt/lfs7/jade-lta')
