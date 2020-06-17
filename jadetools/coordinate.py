190e15ea22429a0a73ab30b233511a4edaa7c8e2
James Bellinger <jbellinger@icecube.wisc.edu>
2020-06-17 15:25:16 -0500

    To run on lta-vm-2 as a "supercron" for worker node LTA modules

---
+++ jadetools/coordinate.py
@@ -0,0 +1,152 @@
+''' To run in a cron on lta-vm-2 as i3admin.
+    Communicates w/ cluster nodes
+    Keeps the appropriate number of bundler and checksum
+     jobs running. '''
+import Utils as U
+
+class coordinate():
+    ''' Ping cluster; query cluster; compare with the desired;
+        execute on cluster (using sudo to jadelta) '''
+    def __init__(self):
+        self.workerpool = []
+        self.workerpool.append('c9-1.icecube.wisc.edu')
+        self.workerpool.append('c8-2.icecube.wisc.edu')
+        self.workerpool.append('c9-2.icecube.wisc.edu')
+        self.workerpool.append('c8-4.icecube.wisc.edu')
+        self.workerpool.append('c9-3.icecube.wisc.edu')
+        self.workerpool.append('c8-5.icecube.wisc.edu')
+        self.workerpool.append('c9-4.icecube.wisc.edu')
+        self.workerpool.append('c8-6.icecube.wisc.edu')
+        self.workerpool.append('c9-5.icecube.wisc.edu')
+        self.workerpool.append('c8-7.icecube.wisc.edu')
+        self.workerpool.append('c9-6.icecube.wisc.edu')
+        self.workerpool.append('c8-8.icecube.wisc.edu')
+        self.workerpool.append('c9-7.icecube.wisc.edu')
+        self.workerpool.append('c8-10.icecube.wisc.edu')
+        self.workerpool.append('c9-8.icecube.wisc.edu')
+        self.workerpool.append('c8-12.icecube.wisc.edu')
+        self.workerpool.append('c9-10.icecube.wisc.edu')
+        self.workerpool.append('c9-11.icecube.wisc.edu')
+        self.workerpool.append('c9-12.icecube.wisc.edu')
+        self.moduleInfo = {}
+        self.moduleInfo['bundler'] = [3, 'bundlerboost']
+        self.moduleInfo['check'] = [5, 'checkboost']
+        self.moduleInfo['delete'] = [1, 'delboost']
+        self.candidatePool = []
+        self.countActiveBundles = 0
+        self.countActiveChecks = 0
+        self.countActiveDeletes = 0
+    #
+    def GetCandidates(self):
+        ''' Ping workerpool machines until we get a list of
+             live ones long enough '''
+        #+
+        # Arguments:	None
+        # Returns:	Nothing
+        # Side Effects:	multiple pings
+        # Relies on:	Utils.getoutputerrorsimplecommand
+        #-
+        # How many do we need?
+        targetCount = 0
+        for p in self.moduleInfo:
+            targetCount = targetCount + self.moduleInfo[p][0]
+        # Find that many, if possible
+        for host in self.workerpool:
+            cmd = ['/bin/ping', '-c1', '-w', '1', host]
+            answer, _, code = U.getoutputerrorsimplecommand(cmd, 1)
+            if code != 0:
+                continue
+            if 'Unreachable' in answer:
+                continue
+            self.candidatePool.append(host)
+            if len(self.candidatePool) >= targetCount:
+                break
+    #
+    def GetInUse(self):
+        ''' Query each of the candidate hosts to see what jadelta
+            jobs they are running.  Increment the counts of modules
+            Note that this does not check modules running on other
+            hosts.  I may change that in the future, since I don't
+            want multiple instances of the deleter--though deleter
+            is a fast module.
+            Return a list of empty hosts '''
+        #+
+        # Arguments:	None
+        # Returns:	list of empty hosts
+        # Side Effects:	multiple calls of script in remote hosts
+        # Relies on:	Utils.getoutputerrorsimplecommand
+        #-
+        emptyList = []
+        for host in self.candidatePool:
+            cmd = ['/usr/bin/ssh', 'i3admin@' + host, '/home/jadelta/getme']
+            answer, _, code = U.getoutputerrorsimplecommand(cmd, 1)
+            if code != 0:
+                continue
+            if 'InterfaceLTA' not in answer and 'bundler' not in answer and 'AutoFiles2' not in answer:
+                emptyList.append(host)
+                continue
+            if 'InterfaceLTA' in answer:
+                self.countActiveChecks = self.countActiveChecks + 1
+            if 'bundler' in answer:
+                self.countActiveBundles = self.countActiveBundles + 1
+            if 'AutoFiles2' in answer:
+                self.countActiveDeletes = self.countActiveDeletes + 1
+        return emptyList
+    #
+    def Launch(self):
+        ''' Find out which modules need to be launched and which
+            hosts are free, and launch the modules 
+            Driver program '''
+        #+
+        # Arguments:	None
+        # Returns:	Nothing
+        # Side Effects:	multiple calls of script in remote hosts
+        # Relies on:	Utils.getoutputerrorsimplecommand
+        #		GetCandidates
+        #		GetInUse
+        #-
+        self.GetCandidates()
+        emptyList = self.GetInUse()
+        # If there are no free hosts, we can't do anything anyway
+        numberFree = len(emptyList)
+        if numberFree <= 0:
+            return
+        whichHost = 0
+        if self.countActiveChecks < self.moduleInfo['check'][0]:
+            for _ in range(self.moduleInfo['check'][0] - self.countActiveChecks):
+                cmd = ['/usr/bin/ssh', 'i3admin@' + emptyList[whichHost], '/home/jadelta/checkboost']
+                answer, error, code = U.getoutputerrorsimplecommand(cmd, 1)
+                if code != 0:
+                    print('coordinate::Launch', cmd, answer, error, code)
+                    return
+                whichHost = whichHost + 1
+                # Are we out of free hosts?
+                if whichHost > numberFree - 1:
+                    return
+        if self.countActiveBundles < self.moduleInfo['bundler'][0]:
+            for _ in range(self.moduleInfo['bundler'][0] - self.countActiveBundles):
+                cmd = ['/usr/bin/ssh', 'i3admin@' + emptyList[whichHost], '/home/jadelta/bundlerboost']
+                answer, error, code = U.getoutputerrorsimplecommand(cmd, 1)
+                if code != 0:
+                    print('coordinate::Launch', cmd, answer, error, code)
+                    return
+                whichHost = whichHost + 1
+                # Are we out of free hosts?
+                if whichHost > numberFree - 1:
+                    return
+        if self.countActiveDeletes < self.moduleInfo['delete'][0]:
+            for _ in range(self.moduleInfo['delete'][0] - self.countActiveDeletes):
+                cmd = ['/usr/bin/ssh', 'i3admin@' + emptyList[whichHost], '/home/jadelta/delboost']
+                answer, error, code = U.getoutputerrorsimplecommand(cmd, 1)
+                if code != 0:
+                    print('coordinate::Launch', cmd, answer, error, code)
+                    return
+                whichHost = whichHost + 1
+                # Are we out of free hosts?
+                if whichHost > numberFree - 1:
+                    return
+        return
+
+if __name__ == '__main__':
+    launch = coordinate()
+    launch.Launch()
