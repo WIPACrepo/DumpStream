302e4d5c79730f5fc4c14a76bade2ba418c9e3aa
James Bellinger <jbellinger@icecube.wisc.edu>
2020-09-25 13:13:24 -0500

    Add more debugging info

--- jadetools/process_directory_v2.sh
+++ jadetools/process_directory_v2.sh
@@ -20,13 +20,15 @@ if [[ ! -d ${directory} ]]; then echo "${directory} is not a visible directory";
 ##
 # The first REST server connection is done by the main program before submitting this job
 # The table in question is FullDirectory
-echo "process_directory_v2.sh ENTRANCE DEBUG $0"
+idate=$( date +%s )
+echo "process_directory_v2.sh ENTRANCE DEBUG $* ${idate}"
 
 ##
 # First execute the file catalog loading script
 if ! env -i ./load_filecatalog "$1"
    then
-      echo "Giving up trying to load $1 into the file catalog"
+      ldate=$( date +%s )
+      echo "Giving up trying to load $1 into the file catalog, ${ldate}"
       exit 1
    fi 
 
@@ -36,7 +38,8 @@ alls=$(env -i ./tell_bundlemaker "$1")
 echo "${alls}" | grep "$1" >& /dev/null
 if [[ $? != 0 ]]
    then
-      echo "Giving up trying to tell the bundler about $1"
+      tdate=$( date +%s )
+      echo "Giving up trying to tell the bundler about $1, ${tdate}"
       echo "${alls}"
       exit 2
    fi
@@ -50,12 +53,13 @@ if [[ -f /usr/bin/curl ]]
   else
     CURL=/bin/curl
   fi
+cdate=$( date +%s )
 manglecom=$( "${dirkey}@LTArequest@${ltaid}" )
 CURLARGS="-sS -X POST -H Content-Type:application/x-www-form-urlencoded"
 target="http://archivecontrol.wipac.wisc.edu/directory/modify/${manglecom}"
 if ! ${CURL} ${CURLARGS} "${target}"
   then
-    echo "FAILURE w/ set status to LTArequest "
+    echo "FAILURE w/ set status to LTArequest ${cdate}"
     exit 5
   fi
 
